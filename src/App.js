614d2b36cbe7019336b04566456b5008772d8c85
Chadi Faour <chadi.m.faour@gmail.com>
2024-09-03 13:43:20 -0400

    my first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h3>hello</h3>
+          <p>Welcome to my website</p>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
